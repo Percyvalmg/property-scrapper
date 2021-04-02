27c0de9a3a8bea5f1df90896723219f21276bb72
Percyvalmg <percyvalg@gmail.com>
2021-04-02 17:25:27 +0200

    Add property data state management

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,21 +1,11 @@
 import {Header} from "./Header";
-import {PropertyView} from "./PropertyView";
-import {PropertyViewEmpty} from "./PropertyViewEmpty";
 import {RemoveItemButton} from "./RemoveItemButton";
 import {Footer} from "./Footer";
-import {usePropertyData} from "./usePropertyData";
-import {PropertyContext, PropertyDataProvider} from "./PropertyDataProvider";
-import {PropertyViewContainer} from "./PropertyViewContainer";
-
+import { PropertyDataProvider} from "./PropertyDataProvider";
 
 export {
     Header,
-    PropertyView,
-    PropertyViewEmpty,
     Footer,
-    usePropertyData,
     PropertyDataProvider,
-    PropertyContext,
-    PropertyViewContainer,
     RemoveItemButton
 }
